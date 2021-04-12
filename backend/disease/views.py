635004e5a9942cc78ab59d6bb63b2a569c32644a
pankajan <pankajan05@gmail.com>
2021-04-12 13:38:25 +0530

    create the machine learning model

--- backend/disease/views.py
+++ backend/disease/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import joblib
+
+reloadModel = joblib.load('./model/RFModelforMPG.pkl')
 
 def index(request):
     return render(request, 'index.html');
 
-def predictDisease():
+def predictDisease(request):
+    if request.method == 'POST':
+        print(request.POST.dict())
     return  None;
