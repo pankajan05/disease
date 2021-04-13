fadc6fe3fadecec01ea1bf0ce97f52b092b111ce
pankajan <pankajan05@gmail.com>
2021-04-13 16:23:21 +0530

    deploy the model successfully

--- backend/disease/views.py
+++ backend/disease/views.py
@@ -2,47 +2,47 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import joblib
 from googletrans import Translator
+import difflib
 
 knn = joblib.load('./model/KNNModel.pkl')
 
-disease=['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis',
+disease=['Fungal infection', 'Allergy', 'Gastroesophageal reflux disease (GERD)', 'Chronic cholestasis',
        'Drug Reaction', 'Peptic ulcer diseae', 'AIDS', 'Diabetes ',
-       'Gastroenteritis', 'Bronchial Asthma', 'Hypertension ', 'Migraine',
+       'Gastroenteritis disease', 'Asthma', 'Hypertension ', 'Migraine',
        'Cervical spondylosis', 'Paralysis (brain hemorrhage)', 'Jaundice',
-       'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A',
-       'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E',
-       'Alcoholic hepatitis', 'Tuberculosis', 'Common Cold', 'Pneumonia',
-       'Dimorphic hemmorhoids(piles)', 'Heart attack', 'Varicose veins',
-       'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia',
+       'Malaria', 'Chicken pox', 'Dengue', 'Typhoid(a bacterial infection)', 'hepatitis A',
+       'Hepatitis B liver infection', 'Hepatitis C virus infection', 'Hepatitis D swelling liver', 'Hepatitis E (A liver disease)',
+       'Alcoholic hepatitis (swelling of the liver)', 'Tuberculosis', 'Common Cold', 'Pneumonia',
+       'Swelling of the blood vessels in the rectum', 'Heart attack', 'Varicose veins',
+       'Body doesn\'t produce enough thyroid hormones', 'Hyperthyroidism', 'Hypoglycemia(low blood sugar)',
        'Osteoarthristis', 'Arthritis',
        '(vertigo) Paroymsal  Positional Vertigo', 'Acne',
        'Urinary tract infection', 'Psoriasis', 'Impetigo']
 
-l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-    'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-    'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-    'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-    'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-    'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-    'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-    'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-    'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-    'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-    'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-    'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-    'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-    'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-    'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-    'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-    'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-    'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-    'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-    'yellow_crust_ooze']
+
+l1=['Body back pain','constipation','abdominal pain','diarrhea','mild_fever','yellowish urine',
+    'yellowish eyes','liver damage','too much fluids in the blood','swelling of stomach',
+    'swelled lymph nodes','malaise','Impaired vision','phlegm','Throat infections',
+    'Redness of eyes','Nasal congestion','discharge mucus from the nose','Body stiffness','chest pain','weakness in limbs',
+    'fast heart rate','pain during bowel movements','pain in anal region','bleeding with stool',
+    'itching around the anus','neck pain','dizziness','cramps of body','bruising','obesity','swollen legs',
+    'swollen blood vessels','puffy face and eyes','That the thyroid gland is large','brittle nails',
+    'swelling of arms, feet, ankles, and legs','excessive hunger','Problems with unmarried relationships','drying and tingling lips',
+    'slurred speech','knee pain','hip joint pain','muscle weakness','Tightness of the neck','swelling joints',
+    'Tightness in body movements','Dizziness during movements','loss of body balance','Body unsteadiness',
+    'weakness of one body side','loss of sense of smell','Pain in the bladder','foul smell of urine',
+    'Need to urinate frequently','releasing gas through the anus','body internal itching','typhus fever',
+    'depression','irritability','muscle pain','inability to think clearly or concentrate','red spots on body','lower stomach pain',
+    'abnormal menstruation','Discolored skin','Tears in the eyes','Excessive hunger','production of abnormal volumes of urine','Hereditary disorders','white mucus come from the nose',
+    'Rust-colored mucus come from the nose','overactivity','change in  eyesight','Transfusion Reactions',
+    'unsafe injection','coma','bleeding in stomach','swelling of the abdomen',
+    'high dosage of alcohol consumption','Excess fluid resulting in body weight gain','blood with mucus','swollen and enlarged veins that occur on the legs and feet',
+    'feel like heart stopped beats','pain when walking','pus-filled pimples','blackheads','scurring','damage to the upper layer of your skin',
+    'the skin turns blue or blue-grey','pitting in Nail','an inflammation of the skin around the nail','blister','Red skin under and around nose',
+    'Yellowish crusting pus builds up']
 
 l2=[]
 
-for i in range(0,len(l1)):
-    l2.append(0)
 
 def index(request):
     return render(request, 'index.html');
@@ -52,17 +52,22 @@ def predictDisease(request):
         print(request.POST.dict())
 
     translator = Translator()
-    data = translator.translate('යුනිකෝඩ් ', dest='en')
 
     psymptoms = [translator.translate(request.POST.dict()['symptom1'], dest='en').text,
                  translator.translate(request.POST.dict()['symptom2'], dest='en').text,
                  translator.translate(request.POST.dict()['symptom3'], dest='en').text]
     print(psymptoms)
 
+    for i in range(0, len(l1)):
+        l2.append(0)
+
     for k in range(0, len(l1)):
         for z in psymptoms:
-            if (z == l1[k]):
+            if difflib.SequenceMatcher(None, z, l1[k]).ratio()*100 > 70:
+                print(l1[k])
                 l2[k] = 1
+
+    print(l2)
 
     inputtest = [l2]
     predict = knn.predict(inputtest)
@@ -79,6 +84,6 @@ def predictDisease(request):
     else:
         print("Not Found")
 
-    data = translator.translate( predicted, dest='si')
+    data = translator.translate(predicted, dest='si')
 
     return render(request, 'result.html', {'data': data.text})
